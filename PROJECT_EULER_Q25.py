13eeda113d21492a35af9245ead7b53f98b65397
Mustafa Tosun <106385903+tosunm20@users.noreply.github.com>
2022-06-23 22:52:58 +0300

    Add files via upload

---
+++ PROJECT_EULER_Q25.py
@@ -0,0 +1,17 @@
+from numpy import size
+
+
+a1 = 1
+a2 = 1
+
+sayac = 2
+while True:
+    a3 = a1 + a2
+    a1 = a2
+    a2 = a3
+    if len(str(a3)) == 1000:
+        print(sayac + 1)
+        break
+    else:
+        sayac += 1
+        continue
